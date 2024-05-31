1358dcc2ca95fc45bb023bdb3560409569050c0c
sankalp-prog <planetsankalp@gmail.com>
2024-05-31 23:11:51 +0530

    Quick Update

--- Heaps/Sort_List_Using_Heaps.py
+++ Heaps/Sort_List_Using_Heaps.py
@@ -3,20 +3,12 @@ import heapq
 input_list = [95, 75, 80, 55, 60, 50, 65]
 output_list = []
         
+heapq.heapify(input_list)
 while len(input_list) > 0:
-    # if I used my max_heap class then I wouldn't need to use the below line after every pop
-    heapq._heapify_max(input_list)
     output_list.append(heapq.heappop(input_list))
 
 print(input_list)
 print(output_list)
 
-"""
-    EXPECTED OUTPUT:
-    ----------------
-    [95, 75, 80, 55, 60, 50, 65]
-    [80, 75, 65, 55, 60, 50]
-    [75, 60, 65, 55, 50]
 
-"""
 
